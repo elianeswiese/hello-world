c1090aa6bcb731615f233caacdda20d331d5478d
elianeswiese <41972629+elianeswiese@users.noreply.github.com>
2018-07-31 15:32:32 -0600

    Update and rename test01.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        if(args[0]=="Hi")
+        if(true)
             System.out.println("Hi, World");
         System.out.println("Hello, World");
     }
