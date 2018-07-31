8bbadcf92c964b484f26a41495901db85b70d8c2
elianeswiese <41972629+elianeswiese@users.noreply.github.com>
2018-07-31 15:30:58 -0600

    Update test01.java

--- test01.java
+++ test01.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        if(args[0]=="Hi")
+            System.out.println("Hi, World");
         System.out.println("Hello, World");
     }
 }
