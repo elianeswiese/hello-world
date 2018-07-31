9078a93ff43e114341ceacb778f36dadc3968462
elianeswiese <41972629+elianeswiese@users.noreply.github.com>
2018-07-31 15:39:40 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        boolean result = true;
+        boolean result;
+        if(args[0] = "Hi"){
+            result = true;
+        }
+        else{
+            result = false;
+        }
         if(result == true){
             System.out.println("Hi, World");
         }
